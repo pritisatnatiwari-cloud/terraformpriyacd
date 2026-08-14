18fd67c83d575c611c0e94aa2b24ad1ac05dba04
Priti tiwari <priti.satna.tiwari@gmail.com>
2026-08-14 23:49:11 +0530

    Initial Terraform Resource Group setup

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
